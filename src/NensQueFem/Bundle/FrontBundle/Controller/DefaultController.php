087e2f7507d32c1b8489c7be3f6502dde0ec64c5
Magí Carbonell <magi2c@gmail.com>
2012-09-02 12:48:46 +0200

    front bundle portal

--- src/NensQueFem/Bundle/FrontBundle/Controller/DefaultController.php
+++ src/NensQueFem/Bundle/FrontBundle/Controller/DefaultController.php
@@ -6,14 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use NensQueFem\Bundle\FrontBundle\Form\SearchActivitatType;
+
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
+    public function portadaAction()
     {
-        return array('name' => $name);
+        $peticion = $this->getRequest();
+
+        $search_arr = array();
+        $form_search   = $this->createForm(new SearchActivitatType(), $search_arr);
+
+
+        return $this->render('NensQueFemFrontBundle:Default:portal.html.twig', array(
+            'entities'      => array(),
+            'form_search'   => $form_search->createView()
+        ));
     }
 }
